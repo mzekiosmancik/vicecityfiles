2416b1a46582c693bfb1a6e0a99aec2c51181c99
Mezo Osmancık <mzekiosmancik@outlook.com>
2026-07-12 13:11:59 -0500

    gtm application

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "**.wp.com" },
-      { protocol: "https", hostname: process.env.WORDPRESS_HOSTNAME ?? "cms.vicecityfiles.com" },
+      { protocol: "https", hostname: process.env.WORDPRESS_HOSTNAME || "cms.vicecityfiles.com" },
     ],
   },
   async headers() {
